1521cf15e7d67b0ff3a5389a4d33d233b1e1fb84
MiguelOteo <miguel_oteo@outlook.com>
2021-11-18 14:51:15 +0100

    SQL update

--- database/Telemed_DB_Struct.sql
+++ database/Telemed_DB_Struct.sql
@@ -12,7 +12,7 @@ CREATE TABLE doctors (
 	doctorId INT PRIMARY KEY AUTO_INCREMENT,
 	doctorIdentification VARCHAR(15) DEFAULT NULL,
 	userId INT NOT NULL,
-	FOREIGN KEY (userId) REFERENCES users(userId)
+	FOREIGN KEY (userId) REFERENCES users(userId) ON DELETE CASCADE
 );
 
 CREATE TABLE patients (
@@ -20,7 +20,7 @@ CREATE TABLE patients (
 	patientId INT PRIMARY KEY AUTO_INCREMENT,
 	patientIdNumber VARCHAR(15) DEFAULT NULL,
 	userId INT NOT NULL,
-	FOREIGN KEY (userId) REFERENCES users(userId),
+	FOREIGN KEY (userId) REFERENCES users(userId) ON DELETE CASCADE,
 	doctorId INT DEFAULT NULL,
-	FOREIGN KEY (doctorId) REFERENCES doctors(doctorId)
+	FOREIGN KEY (doctorId) REFERENCES doctors(doctorId) 
 );
